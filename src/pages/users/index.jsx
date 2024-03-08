ba7e9fefee50c0e6e4e3d589b580c6a66b0f295d
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-08 18:54:57 +0530

    cr_3 user

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -5,13 +5,18 @@ import RightSidebar from "../../component/user/RightSidebar";
 function Users() {
   return (
     <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px] dark:bg-darkblack-700 ">
-      {/* write your code here */}
-      <div className="flex 2xl:flex-row 2xl:space-x-11 flex-col space-y-20">
-        <div className="2xl:flex-1 w-full">
-          <UserFilter />
-          <UsersList />
+      <div className="">
+        <div className=" xl:grid gap-[4rem] grid-cols-6">
+          <div className="col-span-6">
+            <UserFilter />
+          </div>
+          <div className="col-span-6">
+            <UsersList />
+          </div>
+          <div className="col-span-6">
+            <RightSidebar />
+          </div>
         </div>
-        <RightSidebar />
       </div>
     </main>
   );
