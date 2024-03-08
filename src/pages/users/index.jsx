3003f43b8f1314bebf165d1661b3f4b34d8d9f40
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-08 16:27:31 +0530

    ca_3 user

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -7,11 +7,20 @@ function Users() {
     <main className="w-full xl:px-[48px] px-6 pb-6 xl:pb-[48px] sm:pt-[156px] pt-[100px] dark:bg-darkblack-700 ">
       {/* write your code here */}
       <div className="flex 2xl:flex-row 2xl:space-x-11 flex-col space-y-20">
-        <div className="2xl:flex-1 w-full">
-          <UserFilter />
-          <UsersList />
+        <div className="2xl:flex-1  w-full">
+          <div>
+            <UserFilter />
+          </div>
+          <div  className="mt-[3rem]">
+            <RightSidebar />
+          </div>
+          <div  className="mt-[2rem]">
+            <UsersList />
+          </div>
+          <div  className="mt-[2rem]">
+            <RightSidebar />
+          </div>
         </div>
-        <RightSidebar />
       </div>
     </main>
   );
