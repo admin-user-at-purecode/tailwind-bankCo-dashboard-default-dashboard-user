864207d9f18955b87790dd61c102f0fac086426d
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-08 13:01:25 +0530

    la_1_user

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -8,8 +8,8 @@ function Users() {
       {/* write your code here */}
       <div className="flex 2xl:flex-row 2xl:space-x-11 flex-col space-y-20">
         <div className="2xl:flex-1 w-full">
-          <UserFilter />
           <UsersList />
+          <UserFilter />
         </div>
         <RightSidebar />
       </div>
