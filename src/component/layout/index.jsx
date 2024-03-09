12786ef20cb0c1bb6cf2cc89a033a7da0c355614
mohammed067 <mohammedrahaman596@gmail.com>
2024-03-09 15:51:16 +0530

    fr_6 user

--- src/component/layout/index.jsx
+++ src/component/layout/index.jsx
@@ -1,4 +1,3 @@
-
 import Users from "../../pages/users";
 import Sidebar from "./sidebar/index";
 import SidebarV2 from "./sidebar/SidebarV2";
@@ -7,42 +6,36 @@ import HeaderTwo from "./header/HeaderTwo";
 import { useState } from "react";
 import { createContext } from "react";
 
-
 export const ThemeContext = createContext("");
 
 function Layout() {
-    const [sidebar, setSidebar] = useState(true);
-    const [theme, setTheme] = useState(
-        localStorage.getItem("theme") === "" || localStorage.getItem("theme")
-            ? localStorage.getItem("theme")
-            : ""
-    );
+  const [sidebar, setSidebar] = useState(true);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") === "" || localStorage.getItem("theme")
+      ? localStorage.getItem("theme")
+      : ""
+  );
 
-    return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
-            <div
-                className={`layout-wrapper ${
-                    sidebar && "active"
-                }  w-full dark:bg-darkblack-600 `}
-                style={{
-                    borderColor: "#2a313c",
-                }}
-            >
-                <div className="relative flex w-full">
-                    <Sidebar handleActive={() => setSidebar(!sidebar)} />
-                    <SidebarV2 />
-                    <div
-                        className={`body-wrapper flex-1 overflow-x-hidden`}
-                    >
-                        <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
-                        <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
-                        <Users/>
-                    </div>
-                </div>
-            </div>
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div
+        className={`layout-wrapper  w-full dark:bg-darkblack-600 `}
+        style={{
+          borderColor: "#2a313c",
+        }}
+      >
+        <div className="relative flex w-full">
+          <Sidebar handleActive={() => setSidebar(!sidebar)} />
+          <SidebarV2 />
+          <div className={`body-wrapper flex-1 overflow-x-hidden`}>
+            <HeaderOne handleSidebar={() => setSidebar(!sidebar)} />
+            <HeaderTwo handleSidebar={() => setSidebar(!sidebar)} />
+            <Users />
+          </div>
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 export default Layout;
-
